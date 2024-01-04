03b15d39585f2757d0fcc25a1be1e0b9169283db
leojellimann <leo.jellimann@gmail.com>
2024-01-04 15:46:01 +0100

    Display side tab genres

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,17 @@ function App() {
         //2 differents layouts, one for mobile and one for larger screens
         base: `"nav" "main"`,//for mobile only one column
         lg: `"nav nav" "aside main"`//for device larger than 1024px
-      }}>
+      }}
+      templateColumns={{
+        base: '1fr',//When only one column, the main column take full width of the screen
+        lg: '200px 1fr'//Side column take 200px and the main take the rest of the screen
+      }}
+      >
       <GridItem area='nav'>
         <NavBar/>
       </GridItem>
       <Show above='lg'>{/* will be rendered on large screens and above*/}
-        <GridItem area='aside' >
+        <GridItem area='aside' paddingX={5}>
           <GenreList/>
         </GridItem>
       </Show>
