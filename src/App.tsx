d062dfe3ceaf873aaa98a8095c11535f0238a144
leojellimann <leo.jellimann@gmail.com>
2024-01-04 11:33:18 +0100

    Build a reponsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { Button, ButtonGroup } from '@chakra-ui/react'
+import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
 
 function App() {
-  return <Button colorScheme='blue'>Button</Button>
+  return (
+    <Grid 
+      templateAreas={{
+        //2 differents layouts, one for mobile and one for larger screens
+        base: `"nav" "main"`,//for mobile only one column
+        lg: `"nav nav" "aside main"`//for device larger than 1024px
+      }}>
+      <GridItem area='nav' bg='coral'>
+        Nav
+      </GridItem>
+      <Show above='lg'>{/* will be rendered on large screens and above*/}
+        <GridItem area='aside' bg='gold'>
+          Aside
+        </GridItem>
+      </Show>
+      
+      <GridItem area='main' bg='dodgerblue'>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App
