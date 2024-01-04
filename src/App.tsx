0d7529d9cdaa355cb5241968c825215dd530b700
leojellimann <leo.jellimann@gmail.com>
2024-01-04 11:40:36 +0100

    Build a navbar with jelli logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Button, ButtonGroup, Grid, GridItem, Show } from '@chakra-ui/react'
+import NavBar from './components/NavBar';
 
 function App() {
   return (
@@ -8,8 +9,8 @@ function App() {
         base: `"nav" "main"`,//for mobile only one column
         lg: `"nav nav" "aside main"`//for device larger than 1024px
       }}>
-      <GridItem area='nav' bg='coral'>
-        Nav
+      <GridItem area='nav'>
+        <NavBar/>
       </GridItem>
       <Show above='lg'>{/* will be rendered on large screens and above*/}
         <GridItem area='aside' bg='gold'>
